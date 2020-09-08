569d8e02056f3cc2097be217c36ad7aec4103e46
willian-kaminsnki <developer.williansantos@gmail.com>
2020-09-07 22:20:26 -0300

    create a WebSecurity configuration class.

---
+++ src/main/java/com/kamiski/springbootjwt/configuration/secutiry/SecutiryConfig.java
@@ -0,0 +1,32 @@
+package com.kamiski.springbootjwt.configuration.secutiry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecutiryConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+}
