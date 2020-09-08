470c6bfde84b78d17b8e82255c6d77464605ae4d
willian-kaminsnki <developer.williansantos@gmail.com>
2020-09-07 23:50:46 -0300

    add token auth

---
+++ src/main/java/com/kamiski/springbootjwt/configuration/secutiry/AuthenticationByToken.java
@@ -0,0 +1,60 @@
+package com.kamiski.springbootjwt.configuration.secutiry;
+
+import com.kamiski.springbootjwt.domain.User;
+import com.kamiski.springbootjwt.repository.UserRepository;
+import com.kamiski.springbootjwt.service.TokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationByToken extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+    private UserRepository userRepository;
+
+    public AuthenticationByToken(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recoverHeaderToken(httpServletRequest);
+
+        if(tokenService.isTokenValid(token)){
+            authClient(token);
+        }
+
+    }
+
+    private void authClient(String token) {
+
+        Long userId = tokenService.getUserId(token);
+        User user = userRepository.findById(userId).get();
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+    }
+
+    private String recoverHeaderToken(HttpServletRequest httpServletRequest) {
+
+        String token = httpServletRequest.getHeader("Authorization");
+
+        if(token == null || token.isEmpty() || !token.startsWith("Bearer ")){
+            return null;
+        }
+
+        return token.substring(7);
+
+    }
+
+}
